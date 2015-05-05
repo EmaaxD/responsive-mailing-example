3ea18dba9b3e3ae96dd8b05afb1a91ab17cc4068
Koldo Hernández <koldo.hernandez@gmail.com>
2015-05-06 00:58:38 +0200

    Añadido Hogan.js a las plantillas email

--- README.md
+++ README.md
@@ -1,42 +1,12 @@
 # RESPONSIVE EMAILS
 
-Este repositorio contiene un ejemplo sobre cómo enviar emails *responsive* utilizando NodeJS, Sendgrid y 
+Este repositorio contiene un ejemplo sobre cómo enviar emails *responsive* utilizando NodeJS y Sendgrid.
 
-
-
-## Pre-requisitos
-
-Debemos tener instalado como mínimo:
-
-- [NodeJS y npm](http://nodejs.org/)
-
-- [Nodemon](http://nodemon.io)
-
-		$ > npm install -g nodemon
-
-- [Bower](http://bower.io/)
-
-		$ > npm install -g bower
-
-- [Gulp](http://gulpjs.com/)
-
-		$ > npm install -g gulp
-
-De forma opcional, podemos instalar:
-
-- [TSD package manager](https://github.com/Definitelytyped/tsd)
-
-		$ > npm install tsd@next -g
-
-> Nota: puedes ver para qué utilizo TSD en el [post del blog](#)
-
-
-
-
-<br />
 ## TO-DO
 
-- ¿Qué voy a encontrar?
+- Código en desarrollo
 
-- ¿Cómo utilizar el ejemplo?
+- Documentar cómo utilizar el servicio
+
+- Escribir post completo  
 
--- app/app.js
+++ app/app.js
@@ -1,12 +1,37 @@
 /// <reference path="../typings/node/node.d.ts"/>
 /// <reference path="../typings/express/express.d.ts"/>
 
+
+//  MÓDULOS NECESARIOS
+// --------------------
 var express = require('express');
+var path = require('path');
 var app = express();
 var conf = require('./config').config();
 
 var sendgrid  = require('sendgrid')(conf.email.key);
 
+var hogan = require('hogan.js');
+var fs = require('fs');
+
+
+
+//  SETTINGS
+// ----------
+// hogan js templates
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+// obtener la plantilla de email
+var template = fs.readFileSync('./app/views/email.hjs','utf-8');
+
+// compilar la plantilla email
+var compiledTemplate = hogan.compile(template);
+
+
+
+//  RUTAS
+// -------
 // Home
 app.get("/", function(req, res) {
 	res.send("Bienvenido a mi página");
@@ -19,7 +44,7 @@ app.get('/send', function(req, res) {
     to:       'info@koldohernandez.com',
     from:     conf.email.from,
     subject:  'Hola!',
-    text:     'Este es mi primer email desde SendGrid.'
+    html:     compiledTemplate.render({firsName: 'Konelin'})
   };
   
   sendgrid.send(opcionesMail, function(err, json) {
@@ -30,6 +55,13 @@ app.get('/send', function(req, res) {
   });
   
 });
+
+
+// Preview
+app.get('/preview', function(req, res) {
+  res.render('email', {firsName: 'Konelin'});
+});
+
 
 
 //  ARRANCAR EL SERVER
