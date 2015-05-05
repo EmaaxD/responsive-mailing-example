0574055eac82a99d09aef2b5d2286a951546da3f
Koldo <koldo.hm@kimetsport.com>
2015-05-05 18:02:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# RESPONSIVE EMAILS
+
+Este repositorio contiene un ejemplo sobre cómo enviar emails *responsive* utilizando NodeJS, Sendgrid y 
+
+
+
+## Pre-requisitos
+
+Debemos tener instalado como mínimo:
+
+- [NodeJS y npm](http://nodejs.org/)
+
+- [Nodemon](http://nodemon.io)
+
+		$ > npm install -g nodemon
+
+- [Bower](http://bower.io/)
+
+		$ > npm install -g bower
+
+- [Gulp](http://gulpjs.com/)
+
+		$ > npm install -g gulp
+
+De forma opcional, podemos instalar:
+
+- [TSD package manager](https://github.com/Definitelytyped/tsd)
+
+		$ > npm install tsd@next -g
+
+> Nota: puedes ver para qué utilizo TSD en el [post del blog](#)
+
+
+
+
+<br />
+## ¿Qué voy a encontrar?
+
+
+
+<br />
+## ¿Cómo probar el ejemplo?
+
+
---
+++ app/app.js
@@ -0,0 +1,42 @@
+/// <reference path="../typings/node/node.d.ts"/>
+/// <reference path="../typings/express/express.d.ts"/>
+
+var express = require('express');
+var app = express();
+var conf = require('./config').config();
+
+var sendgrid  = require('sendgrid')(conf.email.key);
+
+
+// Home
+app.get("/", function(req, res) {
+	res.send("Bienvenido a mi página");
+});
+  
+// Enviar email
+app.get('/send', function(req, res) {
+  
+  var opcionesMail = {
+    to:       'koldo.hm@kimetsport.com',
+    from:     conf.email.from,
+    subject:  'Hola!',
+    text:     'Este es mi primer email desde SendGrid.'
+  };
+  
+  sendgrid.send(opcionesMail, function(err, json) {
+    if (err) { return console.error(err); }
+    console.log(json);
+    
+    res.send("Email enviado correctamente");
+  });
+  
+});
+
+
+//  ARRANCAR EL SERVER
+// --------------------
+// Escuchar en el puerto configurado
+app.listen(conf.port);
+
+// Log para saber cuando se arranca el servidor
+console.log('App escuchando en el puerto: ' + conf.port);
